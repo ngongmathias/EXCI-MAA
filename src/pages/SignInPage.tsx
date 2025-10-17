8443737da0f332e3498dec0d0eafefaefbec0ce8
DLOADIN <m.david@alustudent.com>
2025-10-17 12:10:56 +0200

    loreee

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/clerk-react';
 export default function SignInPage() {
   return (
     <div className="min-h-[70vh] flex items-center justify-center py-16">
-      <SignIn routing="hash" afterSignInUrl="/admin" />
+      <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up" afterSignInUrl="/admin" />
     </div>
   );
 }
