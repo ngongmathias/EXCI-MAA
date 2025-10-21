d8eca4f6cfc9087d536b4418457255d93b269128
DLOADIN <m.david@alustudent.com>
2025-10-21 20:09:11 +0200

    rifedewrfjdde

--- src/components/pages/contact/ContactForm.tsx
+++ src/components/pages/contact/ContactForm.tsx
@@ -54,7 +54,7 @@ const ContactForm: FC = () => {
   };
 
   return (
-    <section className="py-12">
+    <section className="py-12" id='contact-form'>
       <div className="container mx-auto px-4">
         <div className="text-center mb-12">
           <h2 className="text-3xl font-bold text-gray-900 mb-4">
